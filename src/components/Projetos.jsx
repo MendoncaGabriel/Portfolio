c82754705ee19f86a636f80623b66d7d4c5f887b
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-07-13 11:12:23 -0400

    adicionado projeto

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -5,6 +5,7 @@ import projetoAutoVagas from '../images/p2.webp'
 import projetoPlayTorrent from '../images/playtorrent.png'
 import projetoPaginaInstitucional from '../images/p4.webp'
 import projetoCompactarImagens from '../images/compactarImagens.png'
+import projetoBingo from "../images/bingo.png";
 
 export default function Projetos(){
     return(
@@ -18,12 +19,16 @@ export default function Projetos(){
                     <div class="flex p-10   space-x-10 overflow-auto max-w-[1500px] m-auto ">
     
                         <ProjetoItem img={projetoPlayTorrent} titulo={'Site de Downloads'} gitHubProjeto={'https://github.com/MendoncaGabriel/playtorrent'} verProjeto={'https://www.playtorrent.com.br'} />
+                        <ProjetoItem 
+                            img={projetoBingo} 
+                            titulo={'Sistema de Bingos Online'} 
+                            gitHubProjeto={'https://github.com/MendoncaGabriel/Bingo-Management-System'} 
+                            verProjeto={'https://youtu.be/0818QKHj9gs'} 
+                        />
                         <ProjetoItem img={projetoCompactarImagens} titulo={'Compactar Imagens Online'} gitHubProjeto={'https://github.com/MendoncaGabriel/Compactar-imagem'} verProjeto={'https://mendoncagabriel.github.io/Compactar-imagem/'} />
                         <ProjetoItem img={projetoEbook} titulo={'Pagina de capitura de leeds E-Book'} gitHubProjeto={'https://github.com/MendoncaGabriel/Pagina-Caputura-Ebook'} verProjeto={'https://mendoncagabriel.github.io/Pagina-Caputura-Ebook/'} />
                         <ProjetoItem img={projetoAutoVagas} titulo={'Pagina de vagas online automatica'} gitHubProjeto={'https://github.com/MendoncaGabriel/Auto-Vagas'} verProjeto={'https://mendoncagabriel.github.io/Auto-Vagas'} />
                         <ProjetoItem img={projetoPaginaInstitucional} titulo={'Pagina Institucional'} gitHubProjeto={'https://github.com/MendoncaGabriel/Site-Institucional'} verProjeto={'https://mendoncagabriel.github.io/Site-Institucional/'} />
-
-
 
 
                     </div>
