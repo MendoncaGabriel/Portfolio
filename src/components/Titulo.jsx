508888ce10edebcf17d99ae1f02d40ade0a74017
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-02-24 10:51:49 -0400

    remove all files

--- src/components/Titulo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Titulo({text}){
-    return(
-        <h2 class="minecraft mb-5  text-c1 text-2xl lg:text-4xl text-center md:mb-20 text-blue-800 font-semibold">
-            {text}
-        </h2>
-    )
-}
