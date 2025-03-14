c2100ea5d61c0edd9ab6fbd5c1a182b98f403d85
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-14 17:34:39 -0400

    marcador de pagina na navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,37 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navbar: React.FC = () => {
+  const location = useLocation(); // Obtém a URL atual
+
+  // Lista de rotas definidas na navbar
+  const routes = ["/about", "/articles", "/contact", "/projects"];
+
   return (
     <nav className="z-50 bg-black shadow-black shadow-2xl fixed top-0 left-0 w-full p-4 flex sm:space-x-4 space-x-2 items-center justify-center text-gray-500 border-b-1">
-      <Link to="/" className="hover:text-gray-50 duration-100">INÍCIO</Link>
-      <Link to="/about" className="hover:text-gray-50 duration-100">SOBRE</Link>
-      <Link to="/articles" className="hover:text-gray-50 duration-100">ARTIGOS</Link>
-      <Link to="/contact" className="hover:text-gray-50 duration-100">CONTATO</Link>
-      <Link to="/projects" className="hover:text-gray-50 duration-100">PROJETOS</Link>
+      {[
+        { path: "/", label: "INÍCIO" },
+        { path: "/about", label: "SOBRE" },
+        { path: "/articles", label: "ARTIGOS" },
+        { path: "/contact", label: "CONTATO" },
+        { path: "/projects", label: "PROJETOS" },
+      ].map(({ path, label }) => {
+        // "INÍCIO" será marcado por padrão se a URL atual não estiver na lista de rotas específicas
+        const isActive =
+          location.pathname === path ||
+          (path === "/" && !routes.includes(location.pathname));
+
+        return (
+          <Link
+            key={path}
+            to={path}
+            className={`hover:text-gray-50 duration-100 ${
+              isActive ? "text-gray-100 font-bold" : ""
+            }`}
+          >
+            {label}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
