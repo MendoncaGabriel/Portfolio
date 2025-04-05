2d307a76797d9f8304cfe296111b76d27479fb90
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-05 15:41:44 -0400

    ajuste nos cards

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -30,10 +30,10 @@ export function ArticleCard({
   return (
     <Link
       href={`/artigo/${slug}`}
-      className="cursor-pointer max-w-md bg-white rounded-sm shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300"
+      className="cursor-pointer max-w-md bg-white rounded-md shadow-md overflow-hidden  duration-200 ease-in-out hover:scale-[102%] hover:border-2 border-gray-200"
     >
       {imageUrl && (
-        <div className="w-full h-48 relative">
+        <div className="w-full aspect-square relative">
           <Image
             src={imageUrl}
             alt={title}
@@ -44,8 +44,8 @@ export function ArticleCard({
         </div>
       )}
       <div className="p-2">
-        <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
-        <p className="text-sm text-gray-600 mb-4 line-clamp-3">
+        <h2 className="text-lg font-semibold text-gray-800 mb-2">{title}</h2>
+        <p className="text-sm text-gray-500 mb-4 line-clamp-3">
           {description}
         </p>
         {(author || date) && (
