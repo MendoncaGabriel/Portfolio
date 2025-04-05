9da7aabc4160673a2b73c7e21d1939c49ea2f455
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-05 16:53:40 -0400

    ajuste techLogos

--- src/components/TechLogos.tsx
+++ src/components/TechLogos.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { FaJs, FaReact, FaNodeJs, FaDocker, FaCss3Alt, FaHtml5, FaGitAlt, FaGithub, FaBootstrap } from "react-icons/fa";
 import { SiFastify, SiMysql, SiPrisma, SiMongodb, SiRedis, SiExpress, SiTypescript, SiKnexdotjs, SiRabbitmq, SiJsonwebtokens, SiZod, SiVitess, SiMongoose } from "react-icons/si";
@@ -33,7 +32,7 @@ const icons = [
   { icon: <SiVitess />, colorClassName: "text-[#FCC932]" },
   { icon: <SiMongoose />, colorClassName: "text-[#880000]" },
 
-  // 2x
+  //2
   { icon: <FaHtml5 />, colorClassName: "text-orange-600" },
   { icon: <FaCss3Alt />, colorClassName: "text-blue-600" },
   { icon: <FaJs />, colorClassName: "text-yellow-600" },
@@ -59,32 +58,112 @@ const icons = [
   { icon: <FcLinux />, colorClassName: "" },
   { icon: <SiVitess />, colorClassName: "text-[#FCC932]" },
   { icon: <SiMongoose />, colorClassName: "text-[#880000]" },
+
+  //3
+  { icon: <FaHtml5 />, colorClassName: "text-orange-600" },
+  { icon: <FaCss3Alt />, colorClassName: "text-blue-600" },
+  { icon: <FaJs />, colorClassName: "text-yellow-600" },
+  { icon: <FaNodeJs />, colorClassName: "text-green-600" },
+  { icon: <FaReact />, colorClassName: "text-cyan-400" },
+  { icon: <FaDocker />, colorClassName: "text-blue-500" },
+  { icon: <FaGitAlt />, colorClassName: "text-red-600" },
+  { icon: <FaGithub />, colorClassName: "text-white" },
+  { icon: <SiFastify />, colorClassName: "text-white" },
+  { icon: <SiPrisma />, colorClassName: "text-white" },
+  { icon: <RiTailwindCssFill />, colorClassName: "text-[#00FFFF]" },
+  { icon: <SiMysql />, colorClassName: "text-[#08668E]" },
+  { icon: <BiLogoPostgresql />, colorClassName: "text-[#396C94]" },
+  { icon: <SiMongodb />, colorClassName: "text-[#2DA045]" },
+  { icon: <SiRedis />, colorClassName: "text-[#D93327]" },
+  { icon: <FaBootstrap />, colorClassName: "text-[#7C12F7]" },
+  { icon: <SiExpress />, colorClassName: "text-white" },
+  { icon: <SiTypescript />, colorClassName: "text-[#377CC8]" },
+  { icon: <SiKnexdotjs />, colorClassName: "text-[#E2692D]" },
+  { icon: <SiRabbitmq />, colorClassName: "text-[#F26C29]" },
+  { icon: <SiJsonwebtokens />, colorClassName: "text-[#EA185D]" },
+  { icon: <SiZod />, colorClassName: "text-[#305587]" },
+  { icon: <FcLinux />, colorClassName: "" },
+  { icon: <SiVitess />, colorClassName: "text-[#FCC932]" },
+  { icon: <SiMongoose />, colorClassName: "text-[#880000]" },
+
+  //4
+  { icon: <FaHtml5 />, colorClassName: "text-orange-600" },
+  { icon: <FaCss3Alt />, colorClassName: "text-blue-600" },
+  { icon: <FaJs />, colorClassName: "text-yellow-600" },
+  { icon: <FaNodeJs />, colorClassName: "text-green-600" },
+  { icon: <FaReact />, colorClassName: "text-cyan-400" },
+  { icon: <FaDocker />, colorClassName: "text-blue-500" },
+  { icon: <FaGitAlt />, colorClassName: "text-red-600" },
+  { icon: <FaGithub />, colorClassName: "text-white" },
+  { icon: <SiFastify />, colorClassName: "text-white" },
+  { icon: <SiPrisma />, colorClassName: "text-white" },
+  { icon: <RiTailwindCssFill />, colorClassName: "text-[#00FFFF]" },
+  { icon: <SiMysql />, colorClassName: "text-[#08668E]" },
+  { icon: <BiLogoPostgresql />, colorClassName: "text-[#396C94]" },
+  { icon: <SiMongodb />, colorClassName: "text-[#2DA045]" },
+  { icon: <SiRedis />, colorClassName: "text-[#D93327]" },
+  { icon: <FaBootstrap />, colorClassName: "text-[#7C12F7]" },
+  { icon: <SiExpress />, colorClassName: "text-white" },
+  { icon: <SiTypescript />, colorClassName: "text-[#377CC8]" },
+  { icon: <SiKnexdotjs />, colorClassName: "text-[#E2692D]" },
+  { icon: <SiRabbitmq />, colorClassName: "text-[#F26C29]" },
+  { icon: <SiJsonwebtokens />, colorClassName: "text-[#EA185D]" },
+  { icon: <SiZod />, colorClassName: "text-[#305587]" },
+  { icon: <FcLinux />, colorClassName: "" },
+  { icon: <SiVitess />, colorClassName: "text-[#FCC932]" },
+  { icon: <SiMongoose />, colorClassName: "text-[#880000]" },
+
+  //5
+  { icon: <FaHtml5 />, colorClassName: "text-orange-600" },
+  { icon: <FaCss3Alt />, colorClassName: "text-blue-600" },
+  { icon: <FaJs />, colorClassName: "text-yellow-600" },
+  { icon: <FaNodeJs />, colorClassName: "text-green-600" },
+  { icon: <FaReact />, colorClassName: "text-cyan-400" },
+  { icon: <FaDocker />, colorClassName: "text-blue-500" },
+  { icon: <FaGitAlt />, colorClassName: "text-red-600" },
+  { icon: <FaGithub />, colorClassName: "text-white" },
+  { icon: <SiFastify />, colorClassName: "text-white" },
+  { icon: <SiPrisma />, colorClassName: "text-white" },
+  { icon: <RiTailwindCssFill />, colorClassName: "text-[#00FFFF]" },
+  { icon: <SiMysql />, colorClassName: "text-[#08668E]" },
+  { icon: <BiLogoPostgresql />, colorClassName: "text-[#396C94]" },
+  { icon: <SiMongodb />, colorClassName: "text-[#2DA045]" },
+  { icon: <SiRedis />, colorClassName: "text-[#D93327]" },
+  { icon: <FaBootstrap />, colorClassName: "text-[#7C12F7]" },
+  { icon: <SiExpress />, colorClassName: "text-white" },
+  { icon: <SiTypescript />, colorClassName: "text-[#377CC8]" },
+  { icon: <SiKnexdotjs />, colorClassName: "text-[#E2692D]" },
+  { icon: <SiRabbitmq />, colorClassName: "text-[#F26C29]" },
+  { icon: <SiJsonwebtokens />, colorClassName: "text-[#EA185D]" },
+  { icon: <SiZod />, colorClassName: "text-[#305587]" },
+  { icon: <FcLinux />, colorClassName: "" },
+  { icon: <SiVitess />, colorClassName: "text-[#FCC932]" },
+  { icon: <SiMongoose />, colorClassName: "text-[#880000]" },
+
 ];
 
 const ScrollingLogoAnimation: React.FC = () => {
-  return (
-    <div className="sm:w-2/3 w-full overflow-hidden m-auto">
+   return (
+    <div className="sm:w-2/3 w-full overflow-hidden m-auto" >
       <motion.div
-        className="flex flex-nowrap space-x-8"
+        className="flex flex-nowrap gap-x-10"
         transition={{
-          duration: 20,  // Duração da animação contínua
+          duration: 10 * 5,
           ease: "linear",
           repeat: Infinity,
         }}
         initial={{ x: "0%" }}
-        animate={{ x: "-225%" }} // Move o conteúdo para a esquerda
+        animate={{ x: "-525%" }}
       >
-        {/* Primeira rodada de ícones */}
-
-        {icons.map(({ icon, colorClassName }, iconIndex) => (
+        {[...Array(5)].flatMap(() => icons).map(({ icon, colorClassName }, iconIndex) => (
           <div key={iconIndex} className={`h-10 text-4xl flex-none ${colorClassName}`}>
             {icon}
           </div>
         ))}
-      
       </motion.div>
     </div>
   );
 };
+
 
 export default ScrollingLogoAnimation;
