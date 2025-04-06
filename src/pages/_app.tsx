c0ee2ac273b0c907266a416a39d379147c74b334
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-06 17:07:15 -0400

    feat: add meta google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className="flex-grow mt-20 sm:mt-10">
       <Head>
         <script src="https://cdn.tailwindcss.com?plugins=typography" />
+        <meta name="google-site-verification" content="flm7Rj7-NcWtbgB36OsVnpo8ZxXXqIYKJzvWm9cyNhQ" />
       </Head>
 
       <Navbar />
